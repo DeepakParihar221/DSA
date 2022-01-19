2aaaddf4806940c7ccc661687b0ff5295f48d5b7
DeepakParihar221 <68172840+DeepakParihar221@users.noreply.github.com>
2022-01-19 08:20:20 +0530

    Time: 9 ms (68.62%), Space: 6.9 MB (84.07%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -4,15 +4,15 @@ public:
         int size = s.size(); int ret = 0;
         vector<int> count(26, 0);
         int i = 0; int j = 0; int localMaxFreq = 0; 
-        for(; j<size; j++){
+        for(j=0; j<size; j++){
             count[s[j]-'A']++;
             localMaxFreq = max(localMaxFreq, count[s[j]-'A']);
-            if((j-i+1)-localMaxFreq > k) {
-                ret = max(ret, (j-i));
-                count[s[i]-'A']--;
-                i++;
-                // localMaxFreq = *(max_element(count.begin(), count.end()));
-            }
+            
+            if((j-i+1)-localMaxFreq == k)
+                ret = max(j-i+1, ret);
+            
+            if((j-i+1)-localMaxFreq > k)
+                count[s[i++]-'A']--;
         }
         return max(ret, j-i);
     }
