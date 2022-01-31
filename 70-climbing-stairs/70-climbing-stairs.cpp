9e0f3792a4b99b50e254ada86e40b589b20e84fe
DeepakParihar221 <68172840+DeepakParihar221@users.noreply.github.com>
2022-01-31 10:03:25 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (14.68%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> v(n+1,0);
+        
+        if(n==1 || n==2)
+            return n;
+        v[0] = 0;
+        v[1] = 1;
+        v[2] = 2;
+        for(int i=3;i<=n;++i){
+            v[i] = v[i-1]+v[i-2];
+        }
+        
+        return v[n];
+    }
+};
