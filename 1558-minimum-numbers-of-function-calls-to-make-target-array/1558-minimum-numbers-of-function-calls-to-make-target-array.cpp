0ef7275c99a2727c9517e779b77d0f354d78fdb0
DeepakParihar221 <68172840+DeepakParihar221@users.noreply.github.com>
2022-06-12 09:14:52 +0530

    Time: 130 ms (46.79%), Space: 25.5 MB (16.43%) - LeetHub

--- 1558-minimum-numbers-of-function-calls-to-make-target-array/1558-minimum-numbers-of-function-calls-to-make-target-array.cpp
+++ 1558-minimum-numbers-of-function-calls-to-make-target-array/1558-minimum-numbers-of-function-calls-to-make-target-array.cpp
@@ -2,28 +2,17 @@ class Solution {
 public:
     int minOperations(vector<int>& nums) {
         int n = nums.size();
-        int cntzero = 0;
-        int cnteven = 0;
-        int ans = 0;
-        while(cntzero!=n){
-            cntzero = 0;
-            for(int i=0;i<n;i++){
-                
-                if(nums[i]%2!=0){
-                    nums[i]--;
-                    ans++;
-                }
-                if(nums[i]==0) cntzero++;
+        int maxlen = 1;
+        int bits = 0, res=0;
+        for(int i=0;i<n;i++){
+            bits = 0;
+            while(nums[i]>0){
+                res += nums[i]&1;
+                bits++;
+                nums[i] >>=1;
             }
-            if(cntzero==n) break;
-            else{
-                for(int i=0;i<n;i++){
-                    nums[i] /= 2;
-                }
-                ans++;
-            }
-            
+            maxlen = max(maxlen, bits);
         }
-        return ans;
+        return maxlen-1+res;
     }
 };
