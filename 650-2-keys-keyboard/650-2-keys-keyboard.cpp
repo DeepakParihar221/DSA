f7c86c247434e56cb68f4702a7a6e6a6c85fa386
DeepakParihar221 <68172840+DeepakParihar221@users.noreply.github.com>
2022-06-22 15:42:07 +0530

    Time: 3 ms (77.97%), Space: 5.8 MB (90.06%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
-    int minm = INT_MAX;
-    int dp[1001][1001];
-    void solve(int i, int n, int steps, int prev){
-        if(i==n){
-            minm = min(minm, steps);
-            return;
-        }
-        if(i>n || prev>n) return;
-        
-        if(2*i<n)
-        solve(i+i, n, steps+2, i);
-        solve(i+prev, n, steps+1, prev);
-        return;
-    }
     int minSteps(int n) {
-        if(n==1) return n-1;
-        if(n==2) return n;
-        
-        solve(2, n, 0, 1);
-        return minm+2;
+        int ans = 0, d = 2;
+        while (n > 1) {
+            while (n % d == 0) {
+                ans += d;
+                n /= d;
+            }
+            d++;
+        }
+        return ans;
     }
 };
